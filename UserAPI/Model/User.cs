c2ad8ee20aa2663965f2878964dc2dd15cfb4c47
Solmyr133 <solmyr13369@gmail.com>
2024-10-07 12:10:27 +0200

    Model and database has been created

---
+++ UserAPI/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserAPI.Model
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool License { get; set; }
+    }
+}
---
+++ UserAPI/Model/UserContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserAPI.Model
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() { }
+
+        public UserContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string conn = "server=localhost; database=Users; user=root; password=";
+
+                optionsBuilder.UseMySQL(conn);
+            }
+        }
+
+        DbSet<User> NewUsers { get; set; } = null!;
+    }
+}
